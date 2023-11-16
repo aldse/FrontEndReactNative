876b806fbe363d60347086c96c805c4574b05b32
Eliana Almeida <127983258+aldse@users.noreply.github.com>
2023-11-16 11:08:54 -0300

    Update Condominio.js

--- Condominio.js
+++ Condominio.js
@@ -6,17 +6,120 @@ import { Text, Image, View, TextInput, Switch, TouchableOpacity, TouchableWithou
 import React, { useContext, useState } from 'react';
 import { UtilsContext } from "./Context";
 
-export default function Condominio(props) {
-    return (
-        <>
-            <View
-                style={{
-                    backgroundColor: "black",
-                    height: "100%",
-                    width: "100%",
-                }}>
+import Logo from './logoapenas.png';
 
+const Condominio = (props) => {
+    return (
+        <View style={styles.container}>
+
+            <Image
+                source={Logo}
+                style={styles.logo}
+            />
+            <Text
+                style={styles.bemvindo}>
+                Bem-vindo</Text>
+
+            <View style={styles.topRow}>
+                <View style={styles.v1}></View>
+                <View style={{ marginLeft: 10 }}></View>
+                <View style={styles.v2}></View>
             </View>
-        </>
+
+            <View style={styles.bottomRow}>
+                <View style={styles.v3}></View>
+                <View style={{ marginLeft: 10 }}></View>
+                <View style={styles.v4}></View>
+            </View>
+
+            <View style={styles.footer}>
+                <View style={styles.v5}>
+                    <View style={styles.footerRectangle}></View>
+                    <Image source={Logo} style={styles.logoFooter} />
+                </View>
+            </View>
+        </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        width: 90,
+        height: 80,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+    },
+    bemvindo: {
+        fontSize: 24,
+        color: 'white',
+        textAlign: 'center',
+        marginTop: -90,
+    },
+    topRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 40,
+    },
+    bottomRow: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 20,
+    },
+    v1: {
+        width: 120,
+        height: 200,
+        borderColor: 'white',
+        borderWidth: 2,
+    },
+    v2: {
+        width: 120,
+        height: 200,
+        borderColor: 'white',
+        borderWidth: 2,
+    },
+    v3: {
+        width: 120,
+        height: 200,
+        borderColor: 'white',
+        borderWidth: 2,
+    },
+    v4: {
+        width: 120,
+        height: 200,
+        borderColor: 'white',
+        borderWidth: 2,
+    },
+    footer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 50,
+    },
+    v5: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    footerRectangle: {
+        width: 2,
+        height: 30,
+        backgroundColor: 'white',
+        marginRight: 10,
+    },
+    logoFooter: {
+        width: 60,
+        height: 40,
+    },
+});
+
+export default Condominio;
