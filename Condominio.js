1f94d08122037c10dd0b1907072008deb5d775a5
Eliana Almeida <127983258+aldse@users.noreply.github.com>
2023-11-20 10:01:44 -0300

    Update Condominio.js

--- Condominio.js
+++ Condominio.js
@@ -1,12 +1,17 @@
 // informações como: cada vaga reservada por apartamento, quantidade de apartamentos em cada bloco, custo do condominio mensal, pode ser feito reserva de churrasqueiras, 
 // agendamento de assembleias de condominios, agendamento para a coleta de lixo, realizar denuncias de irregularidades, realizar eleições, gerar boletos para pagamento do condominio,
 // historico das atividades de todos os funcionarios e registro das cameras do porteiro (que pode ser vizualizada apenas pelo sindico)
-
-import { Text, Image, View, TextInput, Switch, TouchableOpacity, TouchableWithoutFeedback, StyleSheet } from "react-native";
-import React, { useContext, useState } from 'react';
-import { UtilsContext } from "./Context";
-
+// #3F3A35 #7D746B #BDBEBE #B28051
+import React from 'react';
+import { Text, Image, View, StyleSheet, TouchableOpacity } from 'react-native';
 import Logo from './logoapenas.png';
+import Logo2 from './logoapenas2.png';
+import TELA1 from './1.png';
+import TELA2 from './2.png';
+import TELA3 from './3.png';
+import TELA4 from './4.png';
+import logonome from './logonome.png';
+
 
 const Condominio = (props) => {
     return (
@@ -16,26 +21,45 @@ const Condominio = (props) => {
                 source={Logo}
                 style={styles.logo}
             />
-            <Text
-                style={styles.bemvindo}>
-                Bem-vindo</Text>
-
+            <Image
+                source={logonome}
+                style={styles.logonome}
+            />
             <View style={styles.topRow}>
-                <View style={styles.v1}></View>
+                <View style={styles.v1}>
+                    <TouchableOpacity style={styles.telaTouchable}>
+                        <Image source={TELA1} style={styles.telaImage} />
+                    </TouchableOpacity>
+                </View>
                 <View style={{ marginLeft: 10 }}></View>
-                <View style={styles.v2}></View>
+                <View style={styles.v2}>
+                    <TouchableOpacity style={styles.telaTouchable}>
+                        <Image source={TELA2} style={styles.telaImage} />
+                    </TouchableOpacity>
+                </View>
             </View>
 
             <View style={styles.bottomRow}>
-                <View style={styles.v3}></View>
+                <View style={styles.v3}>
+                    <TouchableOpacity style={styles.telaTouchable}>
+                        <Image source={TELA3} style={styles.telaImage} />
+                    </TouchableOpacity>
+                </View>
                 <View style={{ marginLeft: 10 }}></View>
-                <View style={styles.v4}></View>
+                <View style={styles.v4}>
+                    <TouchableOpacity style={styles.telaTouchable}>
+                        <Image source={TELA4} style={styles.telaImage} />
+                    </TouchableOpacity>
+                </View>
             </View>
 
             <View style={styles.footer}>
-                <View style={styles.v5}>
-                    <View style={styles.footerRectangle}></View>
-                    <Image source={Logo} style={styles.logoFooter} />
+                <View style={styles.retangulo}>
+                    <Image source={Logo} style={styles.logoFooter1} />
+                    <TouchableOpacity style={styles.button}>
+                        <Text style={styles.buttonText}>Suporte</Text>
+                    </TouchableOpacity>
+                    <Image source={Logo2} style={styles.logoFooter2} />
                 </View>
             </View>
         </View>
@@ -45,22 +69,23 @@ const Condominio = (props) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'black',
+        backgroundColor: '#7D746B',
         justifyContent: 'center',
         alignItems: 'center',
     },
     logo: {
-        width: 90,
-        height: 80,
+        width: 75,
+        height: 65,
         position: 'absolute',
         top: 0,
         left: 0,
+        margin: 6
     },
-    bemvindo: {
-        fontSize: 24,
-        color: 'white',
-        textAlign: 'center',
-        marginTop: -90,
+    logonome: {
+        width: 300,  
+        height: 70,  
+        resizeMode: 'contain', 
+        margin: -20
     },
     topRow: {
         flexDirection: 'row',
@@ -75,26 +100,39 @@ const styles = StyleSheet.create({
     v1: {
         width: 120,
         height: 200,
-        borderColor: 'white',
-        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
     },
     v2: {
         width: 120,
         height: 200,
-        borderColor: 'white',
-        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
     },
     v3: {
         width: 120,
         height: 200,
-        borderColor: 'white',
-        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
     },
     v4: {
         width: 120,
         height: 200,
-        borderColor: 'white',
-        borderWidth: 2,
+        justifyContent: 'center',
+        alignItems: 'center',
+        overflow: 'hidden',
+    },
+    telaTouchable: {
+        width: '100%',
+        height: '100%',
+    },
+    telaImage: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover', // ou 'stretch' se preferir
     },
     footer: {
         flexDirection: 'row',
@@ -104,22 +142,30 @@ const styles = StyleSheet.create({
         bottom: 0,
         left: 0,
         right: 0,
-        height: 50,
+        margin: 5,
     },
-    v5: {
+    retangulo: {
+        borderColor: 'white',
+        borderWidth: 1,
+        width: 250,
+        height: 40,
+        display: 'flex',
         flexDirection: 'row',
+        justifyContent: 'space-between',
         alignItems: 'center',
     },
-    footerRectangle: {
-        width: 2,
+    logoFooter1: {
+        width: 40,
         height: 30,
-        backgroundColor: 'white',
-        marginRight: 10,
     },
-    logoFooter: {
-        width: 60,
-        height: 40,
+    logoFooter2: {
+        width: 40,
+        height: 30,
+    },
+    buttonText: {
+        color: 'white',
     },
 });
 
 export default Condominio;
+
