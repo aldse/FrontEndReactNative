b48fa46dfb8b9c8d95af195d1a3a0d288bb332b2
Eliana Almeida <127983258+aldse@users.noreply.github.com>
2023-11-22 11:13:33 -0300

    Update Cadastro.js

--- Cadastro.js
+++ Cadastro.js
@@ -1,20 +1,29 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import Logo from './logo.png';
 
-const Cadastro = () => {
+const Cadastro = ({ navigation }) => {
   const [nome, setNome] = useState('');
   const [email, setEmail] = useState('');
   const [senha, setSenha] = useState('');
+  const [numeroCofre, setNumeroCofre] = useState('');
 
   const handleSubmit = () => {
     // Aqui você pode adicionar a lógica para enviar os dados do formulário
     console.log('Nome:', nome);
     console.log('E-mail:', email);
     console.log('Senha:', senha);
+    console.log('Número do Cofre:', numeroCofre);
   };
 
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()} style={styles.voltarButton}>
+        <Text style={styles.voltarButtonText}>Voltar</Text>
+      </TouchableOpacity>
+
+      <Image source={Logo} style={styles.logo} />
+
       <Text style={styles.titulo}>Cadastro</Text>
 
       <TextInput
@@ -40,6 +49,13 @@ const Cadastro = () => {
         secureTextEntry
       />
 
+      <TextInput
+        style={styles.input}
+        placeholder="Número do Cofre"
+        value={numeroCofre}
+        onChangeText={(text) => setNumeroCofre(text)}
+      />
+
       <TouchableOpacity style={styles.botao} onPress={handleSubmit}>
         <Text style={styles.textoBotao}>Cadastrar</Text>
       </TouchableOpacity>
@@ -48,11 +64,31 @@ const Cadastro = () => {
 };
 
 const styles = StyleSheet.create({
+  voltarButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    backgroundColor: '#B28051',
+    borderRadius: 5,
+    padding: 10,
+  },
+  voltarButtonText: {
+    color: 'white',
+    fontSize: 16,
+  },
   container: {
     flex: 1,
     backgroundColor: '#7D746B',
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  logo: {
+    width: 75,
+    height: 65,
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    margin: 6,
   },
   titulo: {
     fontSize: 24,
